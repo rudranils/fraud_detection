168e608e6c802e8d31300e80414c7681570e3fba
Rudranil Gupta <gr55890@gmail.com>
2022-03-25 19:10:56 +0530

    Add files via upload

---
+++ data_processing.py
@@ -0,0 +1,12 @@
+
+print("Adding all the data processing code related to fraud detection")
+
+a=5
+b=7
+
+def sum(a,b):
+    c = a+b
+    return c
+
+print("Added a new functoin sum which is correct")
+
